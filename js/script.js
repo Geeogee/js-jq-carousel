952b126d80a12dfb8dca900d93c8fae188f9d4a0
Gioele <gioele.armiento@gmail.com>
2021-03-22 16:00:48 +0100

    VanillaJs refactoring

--- js/script.js
+++ js/script.js
@@ -1,134 +1,91 @@
-// JQuery Version
-
-function nextClick() {
-
-    var activeImg = $(".active");
-    var nextImg = activeImg.next();
-
-    if (activeImg.hasClass("last")) {
-        
-        nextImg = $(".first");
-        activeImg.removeClass("active");
-        nextImg.addClass("active");
-    } else {
-
-        activeImg.removeClass("active");
-        nextImg.addClass("active");
-    }
-    
-}
-
-function prevClick() {
-    var activeImg = $(".active");
-    var prevImg = activeImg.prev();
-
-    if (activeImg.hasClass("first")) {
-        
-        prevImg = $(".last");
-        activeImg.removeClass("active");
-        prevImg.addClass("active");
-    } else {
-
-        activeImg.removeClass("active");
-        prevImg.addClass("active");
-    }
-}
-
-function init() {
-    $(".next").click(nextClick);
-    $(".prev").click(prevClick);
-}
-
-$(document).ready(init);
-
-// VanillaJs version
+// // JQuery Version
 
 // function nextClick() {
-    
-//     var images = document.getElementById("images").children;
-    
-//     for (var i=0; i<images.length; i++) {
-//         if (images[i].classList.contains("active")) {
-//             images[i].classList.remove("active");
-//             var next = i + 1;
-//             if (next > images.length - 1) {
-//                 next = 0;
-//             }
-//             images[next].classList.add("active");
-//             break;
-//         }
+
+//     var activeImg = $(".active");
+//     var nextImg = activeImg.next();
+
+//     if (activeImg.hasClass("last")) {
+        
+//         nextImg = $(".first");
+//         activeImg.removeClass("active");
+//         nextImg.addClass("active");
+//     } else {
+
+//         activeImg.removeClass("active");
+//         nextImg.addClass("active");
 //     }
+    
 // }
 
 // function prevClick() {
+//     var activeImg = $(".active");
+//     var prevImg = activeImg.prev();
 
-//     var images = document.getElementById("images").children;
+//     if (activeImg.hasClass("first")) {
+        
+//         prevImg = $(".last");
+//         activeImg.removeClass("active");
+//         prevImg.addClass("active");
+//     } else {
 
-//     for (var i=images.length - 1; i>=0; i--) {
-
-//         if (images[i].classList.contains("active")) {
-
-//             images[i].classList.remove("active");
-//             var prev = i - 1;
-//             if (prev < 0) {
-
-//                 prev = images.length - 1;
-//             }
-//             images[prev].classList.add("active");
-//             break;
-//         }
+//         activeImg.removeClass("active");
+//         prevImg.addClass("active");
 //     }
 // }
 
-
 // function init() {
-    
-//     var nextButton = document.getElementById("next");
-//     var prevButton = document.getElementById("prev");
-    
-//     nextButton.addEventListener("click", nextClick);
-//     prevButton.addEventListener("click", prevClick);
+//     $(".next").click(nextClick);
+//     $(".prev").click(prevClick);
 // }
 
-// document.addEventListener("DOMContentLoaded", init);
+// $(document).ready(init);
 
 
-
-
-
+// VanillaJs version
 
 //---------- WORK IN PROGRESS --------------//
 
-// Vanilla JS refactoring 
+// querySelector da usare
+// generare pallini con for in base al numero di imamgini
+// gestire immagini con this
 
-// function slideShow(newSlide) {
-//     console.log(newSlide);
-//     var images = document.getElementById("images").children;
-//     for (var i=0; i<images.length; i++) {
-//         if(images[i].classList.contains("active")) {
-//             images[i].classList.remove("active");
-//         }
-//     }
-
-//     if (newSlide > images.length - 1) {
-//         newSlide = 0;
-//     }
-//     images[newSlide].classList.add("active");
+function changeSlide(index, images, actionVal) {
     
-// } 
+    var newIndex = index + actionVal;
+    images[index].classList.remove("active");
 
-// function init() {
-//     var nextButton = document.getElementById("next");
-//     var prevButton = document.getElementById("prev");
-//     var firstSlide = 1;
-//     var newSlide;
-//     nextButton.addEventListener("click", function() {
+    if (newIndex > images.length - 1) {
 
-//         newSlide = firstSlide + 1;
-//         slideShow(newSlide);
-//         firstSlide = newSlide;
-        
-//     })
-// }
+        newIndex = 0;
+    } else if (newIndex < 0) {
 
-// document.addEventListener("DOMContentLoaded", init);
+        newIndex = images.length - 1;
+    }
+
+    images[newIndex].classList.add("active");
+    
+    return newIndex;
+}
+
+function init() {
+    var images = document.querySelectorAll("#images > img");
+    var index = 0;
+    images[index].classList.add("active");
+
+    var nextButton = document.getElementById("next");
+    var prevButton = document.getElementById("prev");
+
+    nextButton.addEventListener("click", function() {
+
+       index = changeSlide(index, images, 1);
+    });
+
+    prevButton.addEventListener("click", function() {
+
+        index = changeSlide(index, images, -1);
+     });
+    
+}
+
+document.addEventListener("DOMContentLoaded", init);
