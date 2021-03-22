ac5c3286ebd07f618bf29e0e83884c397da1553d
Gioele <gioele.armiento@gmail.com>
2021-03-22 13:01:07 +0100

    Active image now changes (still no limit)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 function nextClick() {
     var activeImg = $(".active");
     console.log(activeImg);
+
+    var nextImg = activeImg.next();
+    console.log(nextImg);
+    activeImg.removeClass("active");
+    nextImg.addClass("active");
 }
 
 function prevClick() {
-    console.log("Hello world prev!");
+    var activeImg = $(".active");
+    console.log(activeImg);
+
+    var prevImg = activeImg.prev();
+    console.log(prevImg);
+    activeImg.removeClass("active");
+    prevImg.addClass("active");
 }
 
 function init() {
