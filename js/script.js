c416b29423f95246db75228eab1aa3106f722f67
Gioele <gioele.armiento@gmail.com>
2021-03-22 16:58:35 +0100

    Added dots function in JQuery

--- js/script.js
+++ js/script.js
@@ -1,45 +1,89 @@
-// // JQuery Version
+// JQuery Version
 
-// function nextClick() {
+function nextClick() {
 
-//     var activeImg = $(".active");
-//     var nextImg = activeImg.next();
+    var activeDot = $(".image-dots > i.dot-active");
+    var nextDot = activeDot.next();
 
-//     if (activeImg.hasClass("last")) {
-        
-//         nextImg = $(".first");
-//         activeImg.removeClass("active");
-//         nextImg.addClass("active");
-//     } else {
-
-//         activeImg.removeClass("active");
-//         nextImg.addClass("active");
-//     }
+    var activeImg = $(".active");
+    var nextImg = activeImg.next();
     
-// }
 
-// function prevClick() {
-//     var activeImg = $(".active");
-//     var prevImg = activeImg.prev();
-
-//     if (activeImg.hasClass("first")) {
+    if (activeImg.hasClass("last")) {
         
-//         prevImg = $(".last");
-//         activeImg.removeClass("active");
-//         prevImg.addClass("active");
-//     } else {
+        nextImg = $(".first");
+        nextDot = $(".image-dots > i.first");
 
-//         activeImg.removeClass("active");
-//         prevImg.addClass("active");
-//     }
-// }
+        activeImg.removeClass("active");
+        activeDot.removeClass("dot-active");
 
-// function init() {
-//     $(".next").click(nextClick);
-//     $(".prev").click(prevClick);
-// }
+        nextImg.addClass("active");
+        nextDot.addClass("dot-active");
+    } else {
 
-// $(document).ready(init);
+        activeImg.removeClass("active");
+        activeDot.removeClass("dot-active");
+
+        nextImg.addClass("active");
+        nextDot.addClass("dot-active");
+    }
+    
+}
+
+function prevClick() {
+    var activeDot = $(".image-dots > i.dot-active");
+    var prevDot = activeDot.prev();
+
+    var activeImg = $(".active");
+    var prevImg = activeImg.prev();
+
+    if (activeImg.hasClass("first")) {
+        
+       
+        prevImg = $(".last");
+        prevDot = $(".image-dots > i.last");
+
+        activeImg.removeClass("active");
+        activeDot.removeClass("dot-active");
+
+        prevImg.addClass("active");
+        prevDot.addClass("dot-active"); 
+    } else {
+
+        activeImg.removeClass("active");
+        activeDot.removeClass("dot-active");
+
+        prevImg.addClass("active");
+        prevDot.addClass("dot-active");
+    }
+}
+
+function genDots(n) {
+
+    var dots = "";
+    for (var i=0; i<n; i++) {   
+        
+        dots += "<i class=\"fas fa-circle\" data-number=\"" + i + "\"></i>";
+        $(".image-dots").html(dots);
+        
+    }
+}
+
+function init() {
+    
+    $("#images img:first-child").addClass("active");
+    var imagesLng = $("#images > img").length;
+    genDots(imagesLng);
+    $(".image-dots i:first-child").addClass("dot-active");
+    $(".image-dots i:first-child").addClass("first");
+    $(".image-dots i:last-child").addClass("last");
+
+    $(".next").click(nextClick);
+    $(".prev").click(prevClick);
+
+}
+
+$(document).ready(init);
 
 
 // VanillaJs version
@@ -47,45 +91,46 @@
 //---------- WORK IN PROGRESS --------------//
 
 // querySelector da usare
-// generare pallini con for in base al numero di imamgini
+// generare pallini con for in base al numero di immagini
 // gestire immagini con this
 
-function changeSlide(index, images, actionVal) {
+// function changeSlide(index, images, actionVal) {
     
-    var newIndex = index + actionVal;
-    images[index].classList.remove("active");
+//     var newIndex = index + actionVal;
+//     images[index].classList.remove("active");
 
-    if (newIndex > images.length - 1) {
+//     if (newIndex > images.length - 1) {
 
-        newIndex = 0;
-    } else if (newIndex < 0) {
+//         newIndex = 0;
+//     } else if (newIndex < 0) {
 
-        newIndex = images.length - 1;
-    }
+//         newIndex = images.length - 1;
+//     }
 
-    images[newIndex].classList.add("active");
+//     images[newIndex].classList.add("active");
     
-    return newIndex;
-}
+//     return newIndex;
+// }
 
-function init() {
-    var images = document.querySelectorAll("#images > img");
-    var index = 0;
-    images[index].classList.add("active");
-
-    var nextButton = document.getElementById("next");
-    var prevButton = document.getElementById("prev");
-
-    nextButton.addEventListener("click", function() {
-
-       index = changeSlide(index, images, 1);
-    });
-
-    prevButton.addEventListener("click", function() {
-
-        index = changeSlide(index, images, -1);
-     });
+// function init() {
     
-}
+//     var images = document.querySelectorAll("#images > img");
+//     var index = 0;
+//     images[index].classList.add("active");
 
-document.addEventListener("DOMContentLoaded", init);
+//     var nextButton = document.getElementById("next");
+//     var prevButton = document.getElementById("prev");
+
+//     nextButton.addEventListener("click", function() {
+
+//        index = changeSlide(index, images, 1);
+//     });
+
+//     prevButton.addEventListener("click", function() {
+
+//         index = changeSlide(index, images, -1);
+//      });
+    
+// }
+
+// document.addEventListener("DOMContentLoaded", init);
